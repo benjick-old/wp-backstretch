af4c7ea60dd20046c6893c073a6291ed093313fd
Max Malm <max@malm.me>
2013-08-06 13:29:25 +0200

    added copyright stuffs

--- wp-backstretch.php
+++ wp-backstretch.php
@@ -15,6 +15,22 @@ Download the jQuery plugin from https://github.com/srobbin/jquery-backstretch/zi
 and drop it in the wp-backstretch folder
 */
 
+/*  Copyright 2013  Max Malm  (email : benjick@dumfan.net)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 /*******************************
  GUI 
  *******************************/
